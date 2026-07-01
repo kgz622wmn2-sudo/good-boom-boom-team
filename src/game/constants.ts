2f507c5b82af6eb12e1cbb32d2f9cea79c6859bd
kgz622wmn2-sudo <kgz622wmn2@privaterelay.appleid.com>
2026-07-01 21:52:24 +0800

    Add src/game/constants.ts

---
+++ src/game/constants.ts
@@ -0,0 +1,4 @@
+export const GAME_WIDTH = 1024;
+export const GAME_HEIGHT = 768;
+export const CAMERA_X_FIX = 150;
+export const GAME_FONT = '"Avenir Next Rounded", "SF Pro Rounded", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", ui-rounded, system-ui, sans-serif';
